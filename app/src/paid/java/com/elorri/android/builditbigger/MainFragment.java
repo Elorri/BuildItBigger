3cf087fb315430af47c43fe90fb678222db78777
Elorri <etchemendy.elorri@gmail.com>
2016-02-24 19:06:13 +0100

    Add free andpaid version

---
+++ app/src/paid/java/com/elorri/android/builditbigger/MainFragment.java
@@ -0,0 +1,20 @@
+package com.elorri.android.builditbigger;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Elorri on 24/02/2016.
+ */
+public class MainFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
